3eed1de0efa5e010f648c536af68bd995f160298
Sangeeta Sharma <sangeeta136428sharma@gmail.com>
2024-01-19 08:00:27 +0530

    this is recursion problem

--- Loops and Recursion/2.SumOdd.py
+++ Loops and Recursion/2.SumOdd.py
@@ -6,3 +6,12 @@ def f(n,s):
     return f(n-1,s)
 ans=f(10,0)
 print(ans)
+#another way
+def f(n):
+    if n<=0:
+        return 0
+    elif n%2==1:
+        return n+f(n-1)
+    return f(n-1)
+ans=f(6)
+print(ans)
