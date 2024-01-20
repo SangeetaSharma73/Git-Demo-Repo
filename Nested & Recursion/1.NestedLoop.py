e2c78fc3ceca50e0927ca73b2c863e7cb9f89e0c
Sangeeta Sharma <sangeeta136428sharma@gmail.com>
2024-01-20 09:04:15 +0530

    this is nested 2

--- Nested & Recursion/1.NestedLoop.py
+++ Nested & Recursion/1.NestedLoop.py
@@ -13,4 +13,30 @@ def print_nested_recursively(n, i=0):
     print_nested_recursively(n, i + 1)
 
 # Example: Print the nested pattern with n = 4
+print('USing Recursion')
+print_nested_recursively(4)
+
+
+print(' Using Loop')
+def yourFirstNested(n):
+    for i in range(n):
+        for j in range(n):
+            print(j+1,end=' ')
+        print()
+yourFirstNested(5)
+
+#Using one function
+def print_nested_recursively(n, i=0, j=0):
+    if i == n:
+        return
+
+    if j == n:
+        print()
+        print_nested_recursively(n, i + 1, 0)
+        return
+
+    print(j + 1, end=' ')
+    print_nested_recursively(n, i, j + 1)
+
+# Example: Print the nested pattern with n = 4
 print_nested_recursively(4)
